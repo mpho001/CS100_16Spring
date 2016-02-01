1a59a8addaa35e38a6bc3866ba32928c639804a7
Melinda <mpho001@ucr.edu>
2016-02-01 12:41:00 -0800

    implemented bubble and select

--- Labs/Lab3/sort.h
+++ Labs/Lab3/sort.h
@@ -8,6 +8,7 @@
 
 using namespace std
 
+class Container;
 
 class Sort {
     public:
@@ -23,24 +24,46 @@ class bubbleSort : public Sort {
         bubbleSort() {};
 
         void sort(Container* container) {
-            
+            bool swapped = true;
+            int j = 0;
+            int sz = container->size();
 
+            while(swapped) {
+                swapped = false;
+                ++j;
+                for (int i = 0; i < sz - j; i++) {
+                    if ( container->at(i) > container->at(i + 1) ) {
+                        container->swap(i, i+1);
+                        swapped = true;
+                    }
+                }
+            }
+        }
+};
 
+class selectionSort : public Sort {
+    public:
+        selectionSort() {};
+
+        void sort(Container* container) {
+            int i, j, minIndex;
+            int sz = container->size();
+            for (i = 0; i < sz - 1; i++) {
+                minIndex = i;
+                for (j = i + 1; j < sz; ++j) {
+                    if (container->at(j) < container->at(minIndex) ) {
+                        minIndex = j;
+                    }
+                }
+
+                if (minIndex != i) {
+                    container->swap(i, minIndex);
+                }
+            }
         }
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
+};
 
 #endif
