2f562adad26446d0c7ea741bc43a2a9f06bb073e
Gian Ocampo <gocam001@hammer.cs.ucr.edu>
2016-02-22 13:43:26 -0800

    implemented command.h

--- Labs/Lab5/command.h
+++ Labs/Lab5/command.h
@@ -19,22 +19,40 @@ class Command {
 
 class OpCommand : public Command {
 	//OpCommand Code Here
+	public:
+		OpCommand(double val) {
+			root = new Op(val);
+		}
 };
 
 class AddCommand : public Command {
 	//AddCommand Code Here
+	public:
+		AddCommand(Base* cmd, double val) {
+			root = new Add(cmd, new Op(val);
+		}
 };
 
 class SubCommand : public Command {
 	//SubCommand Code Here
+	public: 
+		SubCommand(Base* cmd, double val) {
+			root = new Sub(cmd, new Op(val));
+		}
 };
 
 class MultCommand : public Command {
 	//MultCommand Code Here
+	MultCommand(Base* cmd, double val) {
+		root = new Mult(cmd, new Op(val));
+	}
 };
 
 class SqrCommand : public Command {
 	//SqrCommand Code Here
+	SqrCommand(double val) {
+		root = new Sqr(new Op(val));
+	}
 };
 
 #endif //__COMMAND_CLASS__
